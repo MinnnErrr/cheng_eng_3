febb59ef24900e083d805eb1e1fce78e9a3542fa
MinnnErrr <lohminer22@gmail.com>
2026-01-18 04:17:54 +0800

    update

--- lib/data/providers/booking/booking_by_date_provider.dart
+++ lib/data/providers/booking/booking_by_date_provider.dart
@@ -6,28 +6,20 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 final bookingPerSlotProvider =
     FutureProvider.family<Map<TimeOfDay, int>, DateTime>((ref, date) async {
       final bookingService = ref.read(bookingServiceProvider);
-      print("DEBUG: Fetching bookings for Date: $date");
       final bookings = await bookingService.getBookingByDate(date);
-      print("DEBUG: Found ${bookings.length} bookings from DB");
 
       final slots = generateTimeSlots();
 
       final Map<TimeOfDay, int> slotCounts = {for (var slot in slots) slot: 0};
 
       for (var booking in bookings) {
-        print("DEBUG: Processing booking time: ${booking.time}");
         final slot = TimeOfDay(
           hour: booking.time.hour,
           minute: booking.time.minute,
         );
         if (slotCounts.containsKey(slot)) {
           slotCounts[slot] = slotCounts[slot]! + 1;
-        } else {
-          // 4. If it prints this, your booking time doesn't match your generated slots
-          print(
-            "WARNING: Booking time $slot does not match any generated slot!",
-          );
-        }
+        } 
       }
 
       return slotCounts;
